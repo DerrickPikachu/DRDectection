260ac68cf7b1c6ac395d1655f161b28d64984047
DerrickPikachu <derrfgh4563@gmail.com>
2021-07-30 23:31:23 +0800

    class ResNet

--- models.py
+++ models.py
@@ -13,42 +13,68 @@ from torch.autograd.function import once_differentiable
 import time
 import functools
 
+import models
+
 
 class ResNet(nn.Module):
-    def __init__(self, pretrained=True):
+    def __init__(self, resnet_depth=50, pretrained=True, feature_extracting=True):
         super(ResNet, self).__init__()
 
-        self.classify = nn.Linear(2048, 5)
+        self.resnet = None
+        if resnet_depth == 50:
+            self.resnet = torchvision.models.resnet50(pretrained)
+        elif resnet_depth == 101:
+            self.resnet = torchvision.models.resnet101(pretrained)
+        elif resnet_depth == 152:
+            self.resnet = torchvision.models.resnet152(pretrained)
 
-        pretrained_model = torchvision.models.__dict__['resnet{}'.format(50)](pretrained=False)
-        self.conv1 = pretrained_model._modules['conv1']
-        self.bn1 = pretrained_model._modules['bn1']
-        self.relu = pretrained_model._modules['relu']
-        self.maxpool = pretrained_model._modules['maxpool']
+        self.setup_feature_extract(feature_extracting)
+        self.resnet.fc = nn.Sequential(nn.Linear(in_features=2048, out_features=5))
 
-        self.layer1 = pretrained_model._modules['layer1']
-        self.layer2 = pretrained_model._modules['layer2']
-        self.layer3 = pretrained_model._modules['layer3']
-        self.layer4 = pretrained_model._modules['layer4']
-
-        self.avgpool = nn.AdaptiveAvgPool2d(1)
-
-        del pretrained_model
+    def setup_feature_extract(self, feature_extracting):
+        if feature_extracting:
+            for parameter in self.resnet.parameters():
+                parameter.requires_grad = False
 
     def forward(self, x):
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu(x)
-        x = self.maxpool(x)
-
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.layer4(x)
-
-        x = self.avgpool(x)
-        # print(x.shape)
-        x = x.view(x.size(0), -1)
-        x = self.classify(x)
-
+        x = self.resnet(x)
         return x
+
+# class ResNet(nn.Module):
+#     def __init__(self, pretrained=True):
+#         super(ResNet, self).__init__()
+#
+#         self.classify = nn.Linear(2048, 5)
+#
+#         pretrained_model = torchvision.models.__dict__['resnet{}'.format(50)](pretrained=False)
+#         self.conv1 = pretrained_model._modules['conv1']
+#         self.bn1 = pretrained_model._modules['bn1']
+#         self.relu = pretrained_model._modules['relu']
+#         self.maxpool = pretrained_model._modules['maxpool']
+#
+#         self.layer1 = pretrained_model._modules['layer1']
+#         self.layer2 = pretrained_model._modules['layer2']
+#         self.layer3 = pretrained_model._modules['layer3']
+#         self.layer4 = pretrained_model._modules['layer4']
+#
+#         self.avgpool = nn.AdaptiveAvgPool2d(1)
+#
+#         del pretrained_model
+#
+#     def forward(self, x):
+#         x = self.conv1(x)
+#         x = self.bn1(x)
+#         x = self.relu(x)
+#         x = self.maxpool(x)
+#
+#         x = self.layer1(x)
+#         x = self.layer2(x)
+#         x = self.layer3(x)
+#         x = self.layer4(x)
+#
+#         x = self.avgpool(x)
+#         # print(x.shape)
+#         x = x.view(x.size(0), -1)
+#         x = self.classify(x)
+#
+#         return x
