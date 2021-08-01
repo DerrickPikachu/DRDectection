4ac604eeb86b104257bde27c2ce30ecfca5a151a
DerrickPikachu <derrfgh4563@gmail.com>
2021-08-01 22:26:15 +0800

    try no normalization

--- parameter.py
+++ parameter.py
@@ -24,13 +24,13 @@ train_transform = transforms.Compose([
     ImgToTorch(),
     transforms.Resize(224),
     # transforms.RandomCrop(224),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    # transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
 test_transform = transforms.Compose([
     ImgToTorch(),
     transforms.Resize(224),
     # transforms.CenterCrop(224),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    # transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
 
 # Test
