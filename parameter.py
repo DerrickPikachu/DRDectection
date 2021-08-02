46adc115e63cef23c209aea2be5a3e7a06239e43
DerrickPikachu <derrfgh4563@gmail.com>
2021-08-02 20:06:28 +0800

    try evenloader

--- parameter.py
+++ parameter.py
@@ -10,14 +10,14 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 # Test
 # epoch = 1
 
-epoch = 20
+epoch = 10
 batch_size = 2
 learning_rate = 0.001
 loss_fn = torch.nn.CrossEntropyLoss()
 
 # TODO: Need to survey the effect of these parameters
 momentum = 0.9
-weight_decay = 5e-4
+weight_decay = 1e-3
 
 
 pretrain_transform = transforms.Compose([
@@ -35,8 +35,8 @@ pretest_transform = transforms.Compose([
 train_transform = transforms.Compose([
     ImgToTorch(),
     transforms.Resize(224),
-    transforms.RandomHorizontalFlip(),
-    transforms.RandomVerticalFlip(),
+    # transforms.RandomHorizontalFlip(),
+    # transforms.RandomVerticalFlip(),
     # transforms.RandomCrop(224),
     # transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
@@ -54,7 +54,7 @@ pretrain_data = PretrainLoader('data', 'train', transform=pretrain_transform)
 pretest_data = PretrainLoader('data', 'test', transform=pretest_transform)
 # train_data = RetinopathyLoader('data', 'train', transform=train_transform)
 # test_data = RetinopathyLoader('data', 'test', transform=test_transform)
-train_data = RetinopathyLoader('data', 'train', transform=train_transform)
+train_data = EvenLoader('data', 'train', transform=train_transform)
 test_data = RetinopathyLoader('data', 'test', transform=test_transform)
 
 loader = {'train': DataLoader(train_data, batch_size=batch_size, shuffle=True, num_workers=4),
